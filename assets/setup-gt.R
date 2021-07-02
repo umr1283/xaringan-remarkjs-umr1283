0bdfddc2e82c39a5276e65bef3c3db7bef80f2a8
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-07-02 09:47:08 +0200

    tweak ggplot2/gt setup and structure

---
+++ assets/setup-gt.R
@@ -0,0 +1,24 @@
+umr <- function(data) {
+  gt::tab_options(
+    data = data,
+    # table.font.color = "#93be25",
+    # column_labels.background.color = "#2a9bd3",
+    column_labels.border.top.color = "#2a9bd3", 
+    column_labels.border.bottom.color = "#2a9bd3", 
+    table_body.border.bottom.color = "#2a9bd3", 
+    table_body.hlines.color = "#2a9bd3",
+    stub.border.color = "#2a9bd3",
+    table.border.bottom.color = "#2a9bd3", 
+    table.border.top.color = "#2a9bd3", 
+    table.border.right.color = "#2a9bd3",
+    table.border.left.color = "#2a9bd3",
+    heading.border.bottom.color = "#2a9bd3",
+    row_group.border.top.color = "#2a9bd3",
+    row_group.border.bottom.color = "#2a9bd3",
+    table_body.border.top.color = "#2a9bd3",
+    summary_row.border.color = "#2a9bd3",
+    source_notes.border.bottom.color = "#2a9bd3",
+    footnotes.border.bottom.color = "#2a9bd3",
+    grand_summary_row.border.color = "#2a9bd3"
+  )
+}
